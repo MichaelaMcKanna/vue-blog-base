63ddcd0f751b85420a5ce81e8e46d52f52406cd6
michaelamckanna <michaela.mckanna@bounteous.com>
2022-02-21 15:01:36 -0600

    complete blog base

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<h1>Home</h1>
+		<div v-if="error">{{ error }}</div>
+		<div v-if="posts.length"><PostList :posts="posts" /></div>
+		<div v-else>Loading...</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PostList from '../components/PostList';
+import getPosts from '../composables/getPosts.js';
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'Home',
+	components: { PostList },
+	setup() {
+		const { load, error, posts } = getPosts();
+
+		load();
+
+		return { posts, error };
+	},
+	// setup() {
+	// 	const writerOne = ref({ name: 'micha', age: 28 });
+	// 	const writerTwo = reactive({ name: 'chance', age: 32 });
+
+	// 	const updateWriterOne = () => {
+	// 		writerOne.value.name = 'Old';
+	// 		writerOne.value.age = 40;
+	// 	};
+	// 	const updateWriterTwo = () => {
+	// 		writerTwo.name = 'Young';
+	// 		writerTwo.age = 20;
+	// 	};
+	// 	return {
+	// 		writerOne,
+	// 		writerTwo,
+	// 		updateWriterOne,
+	// 		updateWriterTwo,
+	// 	};
+	// },
+	// created() {
+	// 	console.log('created');
+	// },
+	// mounted() {
+	// 	console.log('mounted');
+	// },
+};
 </script>
